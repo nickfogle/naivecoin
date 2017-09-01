c7ebe7e6590f4365baff1ad5c8d750a264fe212b
Logan Sease <lsease@gmail.com>
2017-09-01 14:48:35 -0400

    finish run script add logs to ignore

--- run.sh
+++ run.sh
@@ -5,25 +5,28 @@ mkdir logs
 rm -rfd data
 git pull
 npm install
-node bin/naivecoin.js -a 0.0.0.0 --peers http://54.85.162.52:3001 & #> logs/naivecoin.logs
+node bin/naivecoin.js -a 0.0.0.0 --peers http://54.85.162.52:3001 > logs/naivecoin.logs &
 
 sleep 10
 counter=0
 while true
 do
-	echo "polling"
-    scripts/poll_data.sh
+	echo "------ POLLING ------"
+    echo "------ POLLING ------" >> logs/naivecoin.logs
+    scripts/poll_data.sh >> logs/naivecoin.logs &
 
     if [[ $(( $counter % 2 )) == 0 ]];
         then
-        echo "mining"
-        node scripts/mine.js
+        echo "------ MINING ------"
+        echo "------ MINING ------" >> logs/naivecoin.logs
+        node scripts/mine.js >> logs/naivecoin.logs &
     fi
 
     if [[ $(( $counter % 10 )) == 0 ]];
         then
-        echo "heartbeat"
-        node scripts/heartbeat.js
+        echo "------ HEARTBEAT ------"
+        echo "------ HEARTBEAT ------" >> logs/naivecoin.logs
+        node scripts/heartbeat.js >> logs/naivecoin.logs &
     fi
 
     sleep 5
